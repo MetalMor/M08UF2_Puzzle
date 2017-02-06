730cdb1b592faa5702d8e5cf5f558aaff943f9e6
MetalMor <srgark@gmail.com>
2017-02-06 19:08:29 +0100

    20170206: clase de utilidad

---
+++ app/src/main/java/edu/fje/clot/puzzle/statics/Util.java
@@ -0,0 +1,12 @@
+package edu.fje.clot.puzzle.statics;
+
+/**
+ * Created by m0r on 03/02/17.
+ */
+
+public class Util {
+
+    public static int toInteger(double d) {
+        return (int) Math.floor(d);
+    }
+}
