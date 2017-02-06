4a4d1b373be13ff73d435e0d8891142e941c4ce5
MetalMor <srgark@gmail.com>
2017-02-06 19:10:07 +0100

    20170206: MainActivity mejorado

--- app/src/main/java/edu/fje/clot/puzzle/MainActivity.java
+++ app/src/main/java/edu/fje/clot/puzzle/MainActivity.java
@@ -2,6 +2,7 @@ package edu.fje.clot.puzzle;
 
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,29 +12,29 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-import edu.fje.clot.puzzle.audio.MusicService;
+import edu.fje.clot.puzzle.service.audio.MusicService;
+import edu.fje.clot.puzzle.service.image.ImageService;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button play=(Button) findViewById(R.id.PlayButton);
-        play.setOnClickListener(new OnClickListener() {
 
+        setContentView(R.layout.activity_main);
+
+		Button play = (Button) findViewById(R.id.PlayButton);
+        play.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				startService(new Intent(getApplicationContext(), MusicService.class));
+				intentService(MusicService.class);
+				intentService(ImageService.class);
 				startActivity(new Intent(getApplicationContext(), GameActivity.class));
 			}
 		});
-        
     }
     public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu, menu);
-		
 		return super.onCreateOptionsMenu(menu);
 	}
 
@@ -45,5 +46,9 @@ public class MainActivity extends Activity {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private ComponentName intentService(Class service) {
+		return startService(new Intent(getApplicationContext(), service));
 	}
 }
