d4c06d64e67dcdf760a166b69bc633a0aa2e928b
MetalMor <srgark@gmail.com>
2017-02-21 20:34:50 +0100

    20170218: init CarrouselActivity + CarrouselActivity.java, activity_carrousel.xml

---
+++ app/src/main/java/edu/fje/clot/puzzle/CarrouselActivity.java
@@ -0,0 +1,20 @@
+package edu.fje.clot.puzzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by m0r on 21/02/17.
+ */
+
+public class CarrouselActivity extends Activity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
