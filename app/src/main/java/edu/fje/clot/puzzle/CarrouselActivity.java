bd7332b172d8cdd9fbea709f353344019fc4c3ea
MetalMor <srgark@gmail.com>
2017-02-21 21:24:13 +0100

    20170218: CarrouselActivity pendiente de pruebas + animacion de CarrouselActivity con las imagenes guardadas, pendiente añadir un boton en la MainActivity para probarlo

--- app/src/main/java/edu/fje/clot/puzzle/CarrouselActivity.java
+++ app/src/main/java/edu/fje/clot/puzzle/CarrouselActivity.java
@@ -1,9 +1,16 @@
 package edu.fje.clot.puzzle;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.res.TypedArray;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
 import android.widget.ImageView;
 
 /**
@@ -12,8 +19,15 @@ import android.widget.ImageView;
 
 public class CarrouselActivity extends Activity {
 
+    // TODO probar! boton en la MainActivity que te abra esta Activity
+
     ImageView imageView;
-    int counter = 0;
+    int imageIndex = 0;
+
+    AnimatorSet firstAnimation;
+    AnimatorSet lastAnimation;
+
+    private static final String PROPERTY_X_COORD = "x";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +36,51 @@ public class CarrouselActivity extends Activity {
 
         new android.os.Handler().postDelayed(new Runnable() {
             public void run() {
-                // TODO run carrousel...
+                Display display = getWindowManager().getDefaultDisplay();
+                Point size = new Point();
+                final int width = size.x;
+
+                display.getSize(size);
+                imageView.setX(width/4);
+
+                firstAnimation = new AnimatorSet();
+                lastAnimation = new AnimatorSet();
+
+                firstAnimation.playSequentially(
+                        ObjectAnimator.ofFloat(imageView, PROPERTY_X_COORD, width),
+                        ObjectAnimator.ofFloat(imageView, View.ALPHA, 1, 0)
+                );
+
+                firstAnimation.setDuration(500);
+                firstAnimation.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(final Animator firstAnimator) {
+                        super.onAnimationEnd(firstAnimator);
+
+                        setImage();
+                        imageView.setX(0 - imageView.getWidth());
+                        lastAnimation.playSequentially(
+                                ObjectAnimator.ofFloat(imageView, View.ALPHA, 0, 1),
+                                ObjectAnimator.ofFloat(imageView, PROPERTY_X_COORD, width / 4)
+                        );
+                        lastAnimation.setDuration(500);
+                        lastAnimation.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator lastAnimator) {
+                                super.onAnimationEnd(lastAnimator);
+
+                                new android.os.Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        firstAnimator.start();
+                                    }
+                                }, 1000);
+                            }
+                        });
+                        lastAnimation.start();
+                    }
+                });
+                firstAnimation.start();
             }
         }, 1000);
     }
@@ -38,10 +96,15 @@ public class CarrouselActivity extends Activity {
 
     private Drawable getImageByIndex() {
         try {
-            return findImages()[counter];
+            return findImages()[imageIndex];
         } catch (ArrayIndexOutOfBoundsException aioobEx) {
-            counter = 0;
-            return findImages()[counter];
+            imageIndex = 0;
+            return findImages()[imageIndex];
         }
+    }
+
+    private void setImage() {
+        imageView.setImageDrawable(getImageByIndex());
+        imageIndex++;
     }
 }
